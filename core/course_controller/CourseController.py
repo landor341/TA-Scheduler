3800017ed11db3ed54c10c546a7b4b31778c1cae
Landon Faris <larobis4@gmail.com>
2024-11-20 01:42:27 -0600

    Updated pydocs

--- core/course_controller/CourseController.py
+++ core/course_controller/CourseController.py
@@ -1,15 +1,15 @@
-from core.local_data_classes import CourseFormData
+from typing import List
 
-
+from core.local_data_classes import CourseFormData, CourseOverview, CourseRef
 
 
 class CourseController:
     @staticmethod
-    def saveCourse(course_data: CourseFormData, id=None):
+    def saveCourse(course_data: CourseFormData, id: str | None = None) -> str:
         """
         Pre-conditions: courseData contains valid information for creating a course.
-            if id is provided, it matches an existing course in the Courses table
-        Post-conditions: Returns the value of the modified Courses table object. throws an
+            if id is provided, it matches an existing course in the Courses table.
+        Post-conditions: Returns the id of the modified Courses table object. throws an
             IllegalValue exception if id is invalid or the courseData was invalid.
         Side-effects: Inserts or updates a record in the Courses table with the given
             course_data
@@ -17,7 +17,7 @@ class CourseController:
         pass
 
     @staticmethod
-    def getCourse(id):
+    def getCourse(id: str) -> CourseOverview:
         """
         Pre-conditions: id provided is a valid course_id
         Post-conditions: Returns an object containing course, sections, and assignment
@@ -26,12 +26,11 @@ class CourseController:
         """
         pass
 
-
     @staticmethod
-    def searchCourses(course_search_str, semester_id=None):
+    def searchCourses(course_search_str, semester_id=None) -> List[CourseRef]:
         """
         Pre-conditions: Semester is a valid value if given
-        Post-conditions: Returns an array of courses whose title or code
+        Post-conditions: Returns a list of courses whose title or code
             matches the course_search_str.
         Side-effects: N/A
         """
