a172a04c061e75c48a751d14ffbad9b7e7853c83
ctpjr <70709926+ctpjr@users.noreply.github.com>
2024-11-20 21:07:28 -0600

    UserController Skeleton and tests template.

--- core/user_controller/UserController.tests.py
+++ core/user_controller/UserController.tests.py
@@ -1,1 +1,11 @@
 # TODO: Implement user controller tests
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
