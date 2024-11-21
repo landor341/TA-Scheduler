a539acf4d7d2f1a3f3259f7c03196a7b048bd67d
ctpjr <70709926+ctpjr@users.noreply.github.com>
2024-11-21 00:31:48 -0600

    Transferring progress between my pc and laptop for tomorrow's lab.

--- core/user_controller/UserController.py
+++ core/user_controller/UserController.py
@@ -5,64 +5,70 @@
 class UserController:
     def getUser(self):
         """
-        Preconditions: id must be a valid user id.
 
-        Postconditions: Retrieves and returns user details by user_id. If id is invalid, returns an error message.
+        Preconditions: user_id is a valid id matching a record of a user in the Users table.
 
-        Side-effects: none
+        Postconditions: Retrieves the user with id of user_id, along with a list of the courses associated with the user. If the user
+        is a TA then any assignments are also returned embedded into their courses, if the user is an instructor/administrator
+        then any sections they teach are returned embedded into their courses.
 
-        Parameters:
-
-        Id: Integer value of the id of the user to search for.
-
-        Returns: User object from Users with matching user_id and all values corresponding to the fields that are defined in the Users model.
-        """
-        pass
-
-    def saveUser(self, user):
-        """
-        Preconditions: userData contains valid information for the user fields. If an id is provided, it is a valid id.
-
-        Postconditions: Adds a new user record if no id is provided to the Users table or updates the user’s information that has a matching id to the one provided as the argument.
-        Returns an error message if id is invalid or any part of the userData is invalid.
-
-        Side-effects: Inserts or updates a record in the Users table.
+        Side-effects: none.
 
         Parameters:
 
-        UserData: A dictionary object with the appropriately ordered sequence and data type to match the required fields defined in the Users model.
+        User_id: an id matching a user_id stored in the Users table.
 
-        Returns: none
+        Returns: List of Courses, CourseSections, and LabSections linked to the specified user.
         """
         pass
-
-    def deleteUser(self, id):
-        """
-        Preconditions: id provided must be a valid user id.
-
-        Postconditions: Deletes the user with the matching id as the specified id. If no matching records are found, nothing happens.
-
-        Side-effects: Removes a record from the Users table.
-
-        Parameters:
-            Id: Integer value for the user id of the user to be deleted.
-
-        Returns: none
-        """
-        pass
-
+    # These 2 methods are from the old userService class.  If we want them we can implement them.
+    # def saveUser(self, user):
+    #     """
+    #     Preconditions: userData contains valid information for the user fields. If an id is provided, it is a valid id.
+    #
+    #     Postconditions: Adds a new user record if no id is provided to the Users table or updates the user’s information that has a matching id to the one provided as the argument.
+    #     Returns an error message if id is invalid or any part of the userData is invalid.
+    #
+    #     Side-effects: Inserts or updates a record in the Users table.
+    #
+    #     Parameters:
+    #
+    #     UserData: A dictionary object with the appropriately ordered sequence and data type to match the required fields defined in the Users model.
+    #
+    #     Returns: none
+    #     """
+    #     pass
+    #
+    # def deleteUser(self, id):
+    #     """
+    #     Preconditions: id provided must be a valid user id.
+    #
+    #     Postconditions: Deletes the user with the matching id as the specified id. If no matching records are found, nothing happens.
+    #
+    #     Side-effects: Removes a record from the Users table.
+    #
+    #     Parameters:
+    #         Id: Integer value for the user id of the user to be deleted.
+    #
+    #     Returns: none
+    #     """
+    #     pass
+    #
     def searchUser(user_search_string):
         """
+
         Preconditions: N/A
 
-        Postconditions: Returns a list of users matching the search criteria or an error if the string is empty or formatted inappropriately.
+        Postconditions: Returns a list of users matching the search criteria or an error if the string is empty or formatted inappropriately. User does not get any contact information they do not have permission to see to.
 
         Side-effects: none
 
         Parameters:
 
-        User_search_string: String containing valid search parameters in the proper format.
+            User_search_string: String containing valid search parameters in the proper format.
 
-        Returns: a list of matching user objects.
+        Returns: a list of matching user objects, containing only the info needed to populate the explore page.
+
+
         """
         pass
--- core/user_controller/UserController.tests.py
+++ core/user_controller/UserController.tests.py
@@ -1,8 +1,83 @@
 # TODO: Implement user controller tests
+import os
+import django
+from django.test import TestCase
 import unittest
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ta_scheduler.settings')
+django.setup()
+from django.contrib.auth.middleware import get_user
+from ta_scheduler.models import Course, CourseSection, User, TACourseAssignment, LabSection, TALabAssignment
 
+class TestGetUser(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        user1 = User.objects.create_user(
+            first_name='Admin',
+            last_name='User',
+            username='testadmin',
+            email='testadmin@example.com',
+            password='securepassword123',
+            role='Admin',
+            phone='123-456-7890',
+            address='123 Main St, Springfield',
+            office_hours='Mon, Wed, Fri: 10am - 12pm',
+        )
+        user1.save()
+        cls.user2 = User.objects.create_user(
+            first_name='Bob',
+            last_name='Marley',
+            username='newuser22222',
+            email='newuser@example.com',
+            password='securepassword123',
+            role='Instructor',  # Possible values are 'Instructor', 'TA', 'Admin'
+            phone='123-456-7890',
+            address='123 Main St, Springfield',
+            office_hours='Mon, Wed, Fri: 10am - 12pm',
+        )
+        cls.user2.save()
+    def test_username(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.username, 'newuser22222')
 
-class MyTestCase(unittest.TestCase):
+    def test_firstname(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.first_name, 'Bob')
+
+    def test_lastname(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.last_name, 'Marley')
+
+    def test_email(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.email, 'newuser@example.com')
+
+    def test_role(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.role, 'Instructor')
+
+    def test_phone(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.phone, '123-456-7890')
+
+    def test_address(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.address, '123 Main St, Springfield')
+
+    def test_office_hours(self):
+        new_user = get_user(self.user2)
+        self.assertEqual(new_user.office_hours, 'Mon, Wed, Fri: 10am - 12pm')
+
+    def test_NoInput(self):
+        pass
+
+    def test_InvalidInput(self):
+        pass
+
+    def test_validInput(self):
+        pass
+
+class TestSearchUser(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
