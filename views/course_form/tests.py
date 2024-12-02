6c7bf944b2d959d7308e428d938e0a55a10d6680
Landon Faris <larobis4@gmail.com>
2024-12-01 22:12:39 -0600

    Initial notes for tests

--- views/course_form/tests.py
+++ views/course_form/tests.py
@@ -1,7 +1,17 @@
 from django.test import TestCase
 
 
-class TestGetCourseForm(TestCase):
+# Test getting course form for new course (only admin can get)
+# Test getting course form for existing course (only admin can get)
+# Test getting course form for fake course (redirect to home)
+# Test saving new course form with empty data
+# Test saving new course form with malformed data
+# Test saving new course form with good data
+# Test saving existing course with empty data
+# Test saving existing course with malformed data
+# Test saving existing course with good data
+
+class TestGetNewCourseForm(TestCase):
     pass
 
 
