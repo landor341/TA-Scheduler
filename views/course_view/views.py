1692cd93086aa89dccef9842a1967ab3385c75e6
Landon Faris <larobis4@gmail.com>
2024-12-18 21:52:23 -0600

    Course overview now shows correct TA list

--- views/course_view/views.py
+++ views/course_view/views.py
@@ -20,7 +20,6 @@ class CourseView(View):
             # Handle case where course or semester does not exist
             return render(request, '404.html', {'error_message': str(e)})
 
-        tas = [lab.instructor for lab in course_overview.lab_sections if lab.instructor]
         instructors = [section.instructor for section in course_overview.course_sections if section.instructor]
         sections = course_overview.course_sections
         lab_sections = course_overview.lab_sections
@@ -38,7 +37,7 @@ class CourseView(View):
             },
             'sections': sections,
             'lab_sections': lab_sections,
-            'tas': tas,
+            'tas': course_overview.ta_list,
             'instructors': instructors
         }
 
