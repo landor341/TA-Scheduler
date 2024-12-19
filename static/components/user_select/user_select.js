14b2f536712150060fa14dce144e072b3ecc23d2
Landon Faris <larobis4@gmail.com>
2024-12-18 19:13:16 -0600

    Implemented user search component

--- static/components/user_select/user_select.js
+++ static/components/user_select/user_select.js
@@ -1,32 +1,53 @@
 const selectedUsers = [ ];
 
+for (let u of document.getElementById("selected-users-form-control").value.split(",")) {
+    if (u !== "") {
+        fetch(`/api/search/user/?query=${encodeURIComponent(u)}`)
+        .then((response) => response.json())
+        .then((data) => {
+            selectUser(data[0], true)
+        })
+    }
+}
+
 function fetchUsers(query = "") {
     fetch(`/api/search/user/?query=${encodeURIComponent(query)}`)
         .then((response) => response.json())
         .then((data) => {
             const resultsContainer = document.getElementById("dynamic-results");
             resultsContainer.innerHTML = "";
-
-            // Handle no results case
-            if (data.length === 0) {
-                resultsContainer.innerHTML = "<p>No users found.</p>";
-                return;
-            }
+            let displayedUsers = 0;
 
             // Display results
             data.forEach((user) => {
                 if (selectedUsers.find((u) => u.username === user.username)) return; // Don't show selected users
+                if (!displayedUsers) {
+                    const header = document.createElement("div");
+                    header.style = "text-decoration: underline"
+                    header.innerHTML = `
+                        <p style="width: 130px; display: inline-block; text-decoration: underline;">Username</p>Full Name
+                    `;
+                    resultsContainer.appendChild(header);
+                }
+                displayedUsers++;
                 const userDiv = document.createElement("div");
                 userDiv.classList.add("result-item");
                 userDiv.innerHTML = `
                     <li id="${"user-" + user.username}">
-                        <p><a href="/profile/${user.username}">${user.username}</a> ${user.name}
+                        <p><a
+                            href="/profile/${user.username}" 
+                            style=" display: inline-block; width: 100px; text-overflow: clip; margin-right: 30px; color: black;"
+                        >${user.username}</a> ${user.name}
                         <button type="button">Select</button></p>
                     </li>
                 `;
                 userDiv.onclick = function() {selectUser(user)}
                 resultsContainer.appendChild(userDiv);
             });
+            // Handle no results case
+            if (displayedUsers === 0) {
+                resultsContainer.innerHTML = "<p>No users found.</p>";
+            }
         })
         .catch((error) => {
             console.error("Error fetching user data:", error);
@@ -39,24 +60,47 @@ function deselectUser(user) {
     selectedUsers.splice(index, 1)
     document.getElementById("selected-users-form-control").value = selectedUsers.map(u => u.username).join(',')
     document.getElementById("user-select-" + user.username).remove()
+    updateSelectVisibility()
 }
 
-function selectUser(user) {
+function selectUser(user, isFresh=false) {
     selectedUsers.push(user)
-    document.getElementById("selected-users-form-control").value = selectedUsers.map(u => u.username).join(',')
-    document.getElementById("user-"+user.username).remove()
+    updateSelectVisibility()
+    if (!isFresh) {
+        document.getElementById("selected-users-form-control").value = selectedUsers.map(u => u.username).join(',')
+        document.getElementById("user-" + user.username).remove()
+    }
     const selectedContainer = document.getElementById("dynamic-selected-users")
+
+    if (selectedUsers[0] === user) {
+        const header = document.createElement("div");
+        header.style = "text-decoration: underline"
+        header.innerHTML = `
+            <p style="width: 130px; display: inline-block; text-decoration: underline;">Username</p>Full Name
+        `;
+        selectedContainer.appendChild(header);
+    }
+
 
     const userDiv = document.createElement("div");
     userDiv.classList.add("result-item");
     userDiv.innerHTML = `
         <li id="${"user-select-" + user.username}">
-            <p><a href="/profile/${user.username}">${user.username}</a>  ${user.name}
-            <button type="button">Remove</button> </a>
+            <p><a
+                href="/profile/${user.username}" 
+                style=" display: inline-block; width: 100px; text-overflow: clip; margin-right: 30px; color: black;"
+            >${user.username}</a> ${user.name}
+            <button type="button">Select</button></p>
         </li>
     `
     userDiv.onclick = function() { deselectUser(user) }
     selectedContainer.append(userDiv)
+}
+
+function updateSelectVisibility() {
+    console.log(max_users)
+    console.log(selectedUsers.length)
+    document.getElementById("user-selection-container").style = selectedUsers.length >= max_users ? "display: none" : "display: block"
 }
 
 
